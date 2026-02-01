fc53332d0cbb3d08309f927e4aacac4308a93774
Nabil Chouaib <nabil.chouaieb@gmail.com>
2026-02-01 06:49:31 +0100

    fix for build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 import type { NextConfig } from "next";
+import path from 'path';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
   images: {
     remotePatterns: [
       {
